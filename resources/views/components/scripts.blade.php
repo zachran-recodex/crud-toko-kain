44144c471a48d603df81392bb8d29e9b515ddd16
Zachran Razendra <zachranraze@recodex.id>
2024-12-28 02:16:16 +0700

    cleanup unused asset

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -2,27 +2,12 @@
 <script src="{{ asset('js/lib/jquery-3.7.1.min.js') }}"></script>
 <!-- Bootstrap js -->
 <script src="{{ asset('js/lib/bootstrap.bundle.min.js') }}"></script>
-<!-- Apex Chart js -->
-<script src="{{ asset('js/lib/apexcharts.min.js') }}"></script>
 <!-- Data Table js -->
 <script src="{{ asset('js/lib/dataTables.min.js') }}"></script>
 <!-- Iconify Font js -->
 <script src="{{ asset('js/lib/iconify-icon.min.js') }}"></script>
 <!-- jQuery UI js -->
 <script src="{{ asset('js/lib/jquery-ui.min.js') }}"></script>
-<!-- Vector Map js -->
-<script src="{{ asset('js/lib/jquery-jvectormap-2.0.5.min.js') }}"></script>
-<script src="{{ asset('js/lib/jquery-jvectormap-world-mill-en.js') }}"></script>
-<!-- Popup js -->
-<script src="{{ asset('js/lib/magnifc-popup.min.js') }}"></script>
-<!-- Slick Slider js -->
-<script src="{{ asset('js/lib/slick.min.js') }}"></script>
-<!-- prism js -->
-<script src="{{ asset('js/lib/prism.js') }}"></script>
-<!-- file upload js -->
-<script src="{{ asset('js/lib/file-upload.js') }}"></script>
-<!-- audioplayer -->
-<script src="{{ asset('js/lib/audioplayer.js') }}"></script>
 
 <!-- main js -->
 <script src="{{ asset('js/app.js') }}"></script>
